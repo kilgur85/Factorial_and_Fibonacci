bbea4df0e2fba66769a5850fea984cbc8a486fc6
kilgur85 <103890819+kilgur85@users.noreply.github.com>
2022-06-21 12:17:07 +0300

    Теперь всё работает правильно.

--- src/FactorialAndFibonacci/Main.java
+++ src/FactorialAndFibonacci/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Test test = new Test();
-        System.out.println(test.swicthMetods(1,7));
-        System.out.println(Arrays.toString((int[]) test.swicthMetods(2,5)));
+        System.out.println(test.swicthMetods(1,2,5));
+        System.out.println(Arrays.toString((int[]) test.swicthMetods(2,3,7)));
     }
 }
 
